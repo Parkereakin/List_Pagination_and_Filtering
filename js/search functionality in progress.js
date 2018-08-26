283197ab31f7a1148f2f222dbd8ef3be6cd4bc93
Parker Eakin <parker.eakin@gmail.com>
2018-08-25 23:58:21 -0600

    Meets Requirements
    this version is the "meets requirements" for the Techdegree project. Seperate js file added to continue working on search / filter functionality

--- js/search functionality in progress.js
+++ js/search functionality in progress.js
@@ -8,10 +8,59 @@ const page = document.querySelector('.page');
 const searchDiv = page.firstElementChild;
 const studentList = document.querySelector('.student-list');
 const studentItems = document.querySelectorAll('.student-item');
+let studentArray = []
 const numberOfStudents = studentItems.length;
 const perPage = 10;
 let numberOfPages = Math.floor((numberOfStudents / perPage) + 1);
 let currentPage = 1;
+
+
+// Function resets student array to contain all students
+function fullStudentArray() {
+	for (let i = 0; i < numberOfStudents; i++ ) {
+		studentArray.push(studentItems[i]);
+	}
+}
+
+// Function creates and appends search form
+function addSearchForm () {
+	const createSearchDiv = document.createElement('div');
+	createSearchDiv.className = 'student-search';
+	const createSearchInput = document.createElement('input');
+	createSearchInput.placeholder = 'Search for students';
+	const createSearchButton = document.createElement('button');
+	createSearchButton.textContent = 'Search';
+
+	searchDiv.appendChild(createSearchDiv);
+	searchDiv.querySelector('div').appendChild(createSearchInput);
+	searchDiv.querySelector('div').appendChild(createSearchButton);
+}
+
+// Adds functionality to the search form
+function searchForm () {
+	document.addEventListener("input", (e) => {
+		if (e.target.tagName === 'INPUT' && e.target.value != "") {
+
+			for (let i = 0; i < numberOfStudents; i++ ) {
+				let studentName = studentItems[i].querySelector('h3');
+				if (studentName.textContent.includes(e.target.value)) {
+					studentArray.push(studentItems[i]);
+				} else {
+					studentArray.pop(studentItems[i]);
+				}
+			}
+
+		} else {
+			studentArray = []
+			for (let i = 0; i < numberOfStudents; i++ ) {
+				studentArray.push(studentItems[i]);
+			}
+
+		}
+		displayStudents(perPage, currentPage);
+
+	});
+}
 
 
 // Function finds what pagination link is clicked, then shows the corresponding students. Shows students 1 - 10 by default.
@@ -19,12 +68,14 @@ function displayStudents(perPage, currentPage) {
 	const low = ((currentPage * perPage) - perPage)
 	const high = (currentPage * perPage)
 
-	for (let i = 0; i < numberOfStudents; i++) {
+	for (let i = 0; i < studentArray.length; i++) {
 		if (i < low || i >= high) {
-			studentItems[i].style.display = 'none';
+			studentArray[i].style.display = 'none';
+			console.log('none' + studentArray[i])
 		}
 		if (i >= low && i < high) {
-			studentItems[i].style.display = 'block';
+			studentArray[i].style.display = 'block';
+			console.log('block' + studentArray[i])
 		}
 	}
 }
@@ -60,6 +111,12 @@ function paginationLinks() {
 
 // Calls all functions on page load
 document.addEventListener("DOMContentLoaded", () => {
+
+	fullStudentArray()
+
+	searchForm();
+
+	addSearchForm();
 
 	displayStudents(perPage, currentPage);
 
